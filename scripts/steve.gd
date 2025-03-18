d9d6c88e60ab549c595136ac57da951d80c37084
justine <justia2004@gmail.com>
2025-03-18 22:43:28 +0100

    character movement

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -1,28 +1,28 @@
 extends CharacterBody3D
 
+var velocityy = Vector3(0,0,0)
+const SPEED = 6
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = lerp(velocityy.x,0.0,0.1)
+		
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	else:
+		velocity.z = lerp(velocityy.z,0.0,0.1)
+		
 	move_and_slide()
